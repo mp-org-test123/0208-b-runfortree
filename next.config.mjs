9f337cf12d4b2a4efd9542aaef84af553c438b07
shomix <100691266+shomix@users.noreply.github.com>
2023-02-17 12:45:34 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  trailingSlash: true,
   assetPrefix: "https://0208-b-runfortree-71badd.spheron.app",
 };
 
